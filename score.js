795afdd15fc4006c414c11a2c041b30e38eb8f8f
skedwards88 <skedwards88@github.com>
2021-01-23 18:20:02 -0800

    scoring

---
+++ score.js
@@ -0,0 +1,131 @@
+
+class Cluster {
+    constructor({ indexes = new Set(), symbols = new Set() }) {
+        this.indexes = indexes;
+        this.symbols = symbols;
+    }
+
+    get score() {
+        return (this.indexes.size + this.symbols.size);
+    }
+}
+
+class Quadrant {
+    constructor({ color, symbol }) {
+        this.color = color;
+        this.symbol = symbol;
+    }
+}
+board = [
+    [
+        new Quadrant({
+            color: "black",
+            symbol: null
+        }),
+        new Quadrant({
+            color: "blue",
+            symbol: "moon"
+        }),
+        new Quadrant({
+            color: "blue",
+            symbol: "whirl"
+        })
+    ],
+    [
+        new Quadrant({
+            color: "red",
+            symbol: "moon"
+        }),
+        new Quadrant({
+            color: "blue",
+            symbol: "moon"
+        }),
+        new Quadrant({
+            color: "black",
+            symbol: null
+        })
+    ],
+    [
+        new Quadrant({
+            color: "black",
+            symbol: null
+        }),
+        new Quadrant({
+            color: "blue",
+            symbol: "whirl"
+        }),
+        new Quadrant({
+            color: "blue",
+            symbol: "moon"
+        })
+    ],
+    [
+        new Quadrant({
+            color: "blue",
+            symbol: "star"
+        }),
+        new Quadrant({
+            color: "black",
+            symbol: null
+        }),
+        new Quadrant({
+            color: "blue",
+            symbol: "moon"
+        })
+    ]
+]
+
+function findClusters(color, board) {
+    let clusters = []
+    let row_deltas = [-1, 1, 0, 0];
+    let col_deltas = [0, 0, -1, 1];
+
+
+    board.forEach((row, row_index) => row.forEach((square, column_index) => {
+        if (square.color == color) {
+            // Set up for iteration
+            let cluster = new Cluster({});
+            let coordinatesToSearch = [[row_index, column_index]];
+            // Semi-iterative function to search around each coordinate of interest 
+            // for squares of the same color
+            while (coordinatesToSearch.length > 0) {
+                let [search_row, search_column] = coordinatesToSearch.pop();
+                // Record this square in the cluster
+                cluster.indexes.add([search_row, search_column]);
+                cluster.symbols.add(board[search_row][search_column].symbol)
+                // Clear the color from the board so we don't record it more than once
+                board[search_row][search_column].color = 'black'
+                // Search up/down/left right for squares of the same color
+                // If one is found, add it to the list of coordinates to search
+                row_deltas.forEach((row_delta, i) => {
+                    let column_delta = col_deltas[i];
+                    try {
+                        if (board[search_row + row_delta][search_column + column_delta]['color'] == color) {
+                            coordinatesToSearch.push([search_row + row_delta, search_column + column_delta])
+                        }
+                    } catch {
+                        // don't worry if we're at the edge of the board, just quiet the error
+                    }
+                })
+            }
+            // Add this cluster to the list of clusters
+            clusters.push(cluster)
+        }
+    }
+    ))
+    return clusters
+}
+
+function calculateScore(color) {
+
+    let clusters = findClusters(color, board)
+
+    let scores = clusters.map(cluster => cluster.score)
+
+    return Math.max(...scores)
+}
+
+blue = calculateScore('blue')
+red = calculateScore('red')
+console.log(red)
+console.log(blue)
